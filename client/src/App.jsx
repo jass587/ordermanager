8dd3d295ef5e0b151b7288f7d935a3ea3e2fe2a6
Jyoti Singh <js4772520@gmail.com>
2025-07-11 11:47:02 +0530

    added theme for signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import AdminRoutes from './routes/AdminRoutes';
 import UserRoutes from './routes/UserRoutes';
+import SignIn from './pages/signin/SignIn';
+import './assets/scss/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Routes>
+      <Route path="/signin" element={<SignIn />} />
       <Route path="/admin/*" element={<AdminRoutes />} />
       <Route path="/*" element={<UserRoutes />} />
     </Routes>
