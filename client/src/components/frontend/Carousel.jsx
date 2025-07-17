99de4118c34149f8ccb4be640ca4d76aa9790cc3
Jyoti Singh <js4772520@gmail.com>
2025-07-17 19:07:02 +0530

    carousel fix

--- client/src/components/frontend/Carousel.jsx
+++ client/src/components/frontend/Carousel.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Item = ({ item, index }) => (
   <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
     <Link to={item.to}>
-      <img src={item.img} className="img-fluid" style={{ width : '100vw'}} alt={item.title} />
+      <img src={item.img} className="img-fluid" style={{ width: "100vw" }} alt={item.title} />
       {(item.title || item.description) && (
         <div className="carousel-caption d-none d-md-block">
           {item.title && <h5>{item.title}</h5>}
@@ -23,20 +23,26 @@ const Indicator = ({ item, index }) => (
 );
 
 const Banner = (props) => {
+  // Clone and reorder the data array
+  const reorderedData = [
+    ...(props.data[2] ? [props.data[2]] : []), // third item as first
+    ...props.data.filter((_, index) => index !== 2), // rest excluding third
+  ];
+
   return (
     <div
       id={props.id}
       className={`carousel slide ${props.className}`}
       data-bs-ride="carousel"
-      style={{ minHeight: 100, width : '100vw'}}
+      style={{ minHeight: 100, width: "100vw" }}
     >
       <ol className="carousel-indicators">
-        {props.data.map((item, index) => (
+        {reorderedData.map((item, index) => (
           <Indicator item={props.id} index={index} key={index} />
         ))}
       </ol>
       <div className="carousel-inner">
-        {props.data.map((item, index) => (
+        {reorderedData.map((item, index) => (
           <Item item={item} index={index} key={index} />
         ))}
       </div>
