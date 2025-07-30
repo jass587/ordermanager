67fb43ca5425d4d87b711defd01b97a5e1f9bcb7
Jyoti Singh <js4772520@gmail.com>
2025-07-30 18:34:59 +0530

    build db changes

--- server/config/database.js
+++ server/config/database.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'admin123',
+    database: process.env.DB_NAME || 'ecommerce_db',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Required for Render
+      }
+    }
   }
 };
